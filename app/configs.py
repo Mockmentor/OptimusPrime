4862ad69f77ad82e8817a677a1f36be752a39bd9
m.oynoshev <m.oynoshev@tinkoff.ru>
2023-06-26 19:04:33 +0300

    Add volume for audio files

--- app/configs.py
+++ app/configs.py
@@ -5,11 +5,11 @@ class Settings(BaseSettings):
     db_url: str
     db_echo_sql: bool = False
 
-    audio_files_dir: str = '/audio_files'
+    audio_files_dir: str
     audio_files_format: str = 'wav'
 
-    unicron_host: str = 'localhost'
-    unicron_port: int = 6969
+    unicron_host: str = '0.0.0.0'
+    unicron_port: int = 8000
 
     class Config:
         env_file = '.env'
